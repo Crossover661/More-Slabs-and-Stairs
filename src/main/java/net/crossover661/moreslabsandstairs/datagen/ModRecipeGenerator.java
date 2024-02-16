6e50f59729f583b2b49d0c6b8af98f30431a425b
Crossover661 <sanjay.k.srikanth@gmail.com>
2024-02-16 15:11:32 -0800

    Add rebuilding recipes and fix display name error with terracotta stairs

--- src/main/java/net/crossover661/moreslabsandstairs/datagen/ModRecipeGenerator.java
+++ src/main/java/net/crossover661/moreslabsandstairs/datagen/ModRecipeGenerator.java
@@ -9,6 +9,7 @@ import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.Recipe;
+import net.minecraft.recipe.ShapedRecipe;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.util.Identifier;
 
@@ -18,32 +19,41 @@ public class ModRecipeGenerator extends FabricRecipeProvider {
     public ModRecipeGenerator(FabricDataOutput output) {
         super(output);
     }
-    private void genRecipes(Consumer<RecipeJsonProvider> exporter, RecipeCategory category, ItemConvertible block, ItemConvertible slab, ItemConvertible stairs)
-    {
+    private void genRebuildingRecipe(Consumer<RecipeJsonProvider> exporter, RecipeCategory category, ItemConvertible block, ItemConvertible slab){
+        ShapedRecipeJsonBuilder.create(category, block, 2).pattern("xx").pattern("xx")
+                .input('x', slab)
+                .criterion(FabricRecipeProvider.hasItem(block), FabricRecipeProvider.conditionsFromItem(block))
+                .offerTo(exporter);
+    }
+    private void genRecipes(Consumer<RecipeJsonProvider> exporter, RecipeCategory category, ItemConvertible block, ItemConvertible slab, ItemConvertible stairs) {
+        // generates crafting recipes only
         offerSlabRecipe(exporter, category, slab, block);
         createStairsRecipe(stairs, Ingredient.ofItems(block))
                 .criterion(hasItem(block), conditionsFromItem(block))
                 .offerTo(exporter, new Identifier(getRecipeName(stairs)));
+        genRebuildingRecipe(exporter, category, block, slab);
     }
     private void genRecipesStonecutter(Consumer<RecipeJsonProvider> exporter, RecipeCategory category, ItemConvertible block,
-                                      ItemConvertible slab, ItemConvertible stairs)
-    {
+                                      ItemConvertible slab, ItemConvertible stairs) {
+        // generates crafting and stonecutter recipes
         offerSlabRecipe(exporter, category, slab, block);
         createStairsRecipe(stairs, Ingredient.ofItems(block))
                 .criterion(hasItem(block), conditionsFromItem(block))
                 .offerTo(exporter, new Identifier(getRecipeName(stairs)));
         offerStonecuttingRecipe(exporter, category, slab, block, 2);
         offerStonecuttingRecipe(exporter, category, stairs, block);
+        genRebuildingRecipe(exporter, category, block, slab);
     }
     private void genOnlyStonecutterRecipes(Consumer<RecipeJsonProvider> exporter, RecipeCategory category, ItemConvertible block,
-                                       ItemConvertible slab, ItemConvertible stairs)
-    {
+                                       ItemConvertible slab, ItemConvertible stairs) {
+        // generates only stonecutter recipes
         offerStonecuttingRecipe(exporter, category, slab, block, 2);
         offerStonecuttingRecipe(exporter, category, stairs, block);
     }
     @Override
     public void generate(Consumer<RecipeJsonProvider> exporter)
     {
+        // generate recipes for all modded blocks
         genRecipesStonecutter(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.IRON_BLOCK, ModBlocks.IRON_SLAB, ModBlocks.IRON_STAIRS);
         genRecipesStonecutter(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.GOLD_BLOCK, ModBlocks.GOLD_SLAB, ModBlocks.GOLD_STAIRS);
         genRecipesStonecutter(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.DIAMOND_BLOCK, ModBlocks.DIAMOND_SLAB, ModBlocks.DIAMOND_STAIRS);
@@ -90,5 +100,63 @@ public class ModRecipeGenerator extends FabricRecipeProvider {
         genRecipesStonecutter(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.PURPLE_TERRACOTTA, ModBlocks.PURPLE_TERRACOTTA_SLAB, ModBlocks.PURPLE_TERRACOTTA_STAIRS);
         genRecipesStonecutter(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.MAGENTA_TERRACOTTA, ModBlocks.MAGENTA_TERRACOTTA_SLAB, ModBlocks.MAGENTA_TERRACOTTA_STAIRS);
         genRecipesStonecutter(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.PINK_TERRACOTTA, ModBlocks.PINK_TERRACOTTA_SLAB, ModBlocks.PINK_TERRACOTTA_STAIRS);
+
+        // generate re-crafting recipes for all vanilla slabs
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.OAK_PLANKS, Blocks.OAK_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.SPRUCE_PLANKS, Blocks.SPRUCE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.BIRCH_PLANKS, Blocks.BIRCH_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.JUNGLE_PLANKS, Blocks.JUNGLE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.ACACIA_PLANKS, Blocks.ACACIA_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.DARK_OAK_PLANKS, Blocks.DARK_OAK_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.MANGROVE_PLANKS, Blocks.MANGROVE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.CHERRY_PLANKS, Blocks.CHERRY_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.BAMBOO_PLANKS, Blocks.BAMBOO_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.BAMBOO_MOSAIC, Blocks.BAMBOO_MOSAIC_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.CRIMSON_PLANKS, Blocks.CRIMSON_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.WARPED_PLANKS, Blocks.WARPED_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.STONE, Blocks.STONE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.SMOOTH_STONE, Blocks.SMOOTH_STONE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.GRANITE, Blocks.GRANITE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.POLISHED_GRANITE, Blocks.POLISHED_GRANITE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.DIORITE, Blocks.DIORITE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.POLISHED_DIORITE, Blocks.POLISHED_DIORITE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.ANDESITE, Blocks.ANDESITE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.POLISHED_ANDESITE, Blocks.POLISHED_ANDESITE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.COBBLESTONE, Blocks.COBBLESTONE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.MOSSY_COBBLESTONE, Blocks.MOSSY_COBBLESTONE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.STONE_BRICKS, Blocks.STONE_BRICK_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.MOSSY_STONE_BRICKS, Blocks.MOSSY_STONE_BRICK_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.BRICKS, Blocks.BRICK_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.END_STONE_BRICKS, Blocks.END_STONE_BRICK_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.NETHER_BRICKS, Blocks.NETHER_BRICK_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.RED_NETHER_BRICKS, Blocks.RED_NETHER_BRICK_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.SANDSTONE, Blocks.SANDSTONE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.CUT_SANDSTONE, Blocks.CUT_SANDSTONE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.SMOOTH_SANDSTONE, Blocks.SMOOTH_SANDSTONE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.RED_SANDSTONE, Blocks.RED_SANDSTONE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.CUT_RED_SANDSTONE, Blocks.CUT_RED_SANDSTONE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.SMOOTH_RED_SANDSTONE, Blocks.SMOOTH_RED_SANDSTONE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.QUARTZ_BLOCK, Blocks.QUARTZ_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.SMOOTH_QUARTZ, Blocks.SMOOTH_QUARTZ_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.PURPUR_BLOCK, Blocks.PURPUR_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.PRISMARINE, Blocks.PRISMARINE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.PRISMARINE_BRICKS, Blocks.PRISMARINE_BRICK_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.DARK_PRISMARINE, Blocks.DARK_PRISMARINE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.BLACKSTONE, Blocks.BLACKSTONE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.POLISHED_BLACKSTONE, Blocks.POLISHED_BLACKSTONE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.POLISHED_BLACKSTONE_BRICKS, Blocks.POLISHED_BLACKSTONE_BRICK_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.CUT_COPPER, Blocks.CUT_COPPER_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.EXPOSED_CUT_COPPER, Blocks.EXPOSED_CUT_COPPER_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.WEATHERED_CUT_COPPER, Blocks.WEATHERED_CUT_COPPER_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.OXIDIZED_CUT_COPPER, Blocks.OXIDIZED_CUT_COPPER_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.WAXED_CUT_COPPER, Blocks.WAXED_CUT_COPPER_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.WAXED_EXPOSED_CUT_COPPER, Blocks.WAXED_EXPOSED_CUT_COPPER_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.WAXED_WEATHERED_CUT_COPPER, Blocks.WAXED_WEATHERED_CUT_COPPER_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.WAXED_OXIDIZED_CUT_COPPER, Blocks.WAXED_OXIDIZED_CUT_COPPER_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.COBBLED_DEEPSLATE, Blocks.COBBLED_DEEPSLATE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.POLISHED_DEEPSLATE, Blocks.POLISHED_DEEPSLATE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.DEEPSLATE_BRICKS, Blocks.DEEPSLATE_BRICK_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.DEEPSLATE_TILES, Blocks.DEEPSLATE_TILE_SLAB);
+        genRebuildingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, Blocks.MUD_BRICKS, Blocks.MUD_BRICK_SLAB);
     }
 }
